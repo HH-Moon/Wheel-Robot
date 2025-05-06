6db8cd6ef65d346aab92c43acd615c518e61c113
HHww__ <504293001@qq.com>
2025-05-06 16:45:02 +0800

    前铲修改

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import uptech
 import time
 
 def down_platform_act():
-    up.CDS_SetSpeed(1, -730)
-    up.CDS_SetSpeed(2, -750)
+    up.CDS_SetSpeed(1, -730) #730
+    up.CDS_SetSpeed(2, -750) #750
     time.sleep(0.3)
-    up.CDS_SetAngle(3, 330, 512)
-    up.CDS_SetAngle(4, 700, 512)
-    time.sleep(1)
-    up.CDS_SetAngle(3, 594, 712) #550
-    up.CDS_SetAngle(4, 388, 712) #512
+    up.CDS_SetAngle(3, 100, 512) #300
+    up.CDS_SetAngle(4, 930, 512) #730
+    time.sleep(0.5)
+    up.CDS_SetAngle(3, 412, 712) #550  664
+    up.CDS_SetAngle(4, 612, 712) #512  338
     time.sleep(1)
 
 up = uptech.UpTech()
@@ -28,8 +28,15 @@ up.LCD_SetFont(up.FONT_6X10)
 
 while True:
     down_platform_act()
-    # up.CDS_SetAngle(3, 512, 512)
-    # up.CDS_SetAngle(4, 512, 512)
+    # up.CDS_SetAngle(3, 100, 512) #300
+    # up.CDS_SetAngle(4, 930, 512) #730
+    # time.sleep(0.5)
+    # up.CDS_SetAngle(3, 412, 712) #550  664
+    # up.CDS_SetAngle(4, 612, 712) #512  338
+    # time.sleep(1)
+    # up.CDS_SetSpeed(1, -730)  # 730
+    # up.CDS_SetSpeed(2, -750)  # 750
+
 
 
 
