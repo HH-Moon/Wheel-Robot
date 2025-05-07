1033df206b0be8b4a4d1e0980216fc9fcff23c81
HHww__ <504293001@qq.com>
2025-05-07 18:00:57 +0800

    上台2.0

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import time
 def down_platform_act():
     up.CDS_SetSpeed(1, -730) #730
     up.CDS_SetSpeed(2, -750) #750
-    time.sleep(0.3)
-    up.CDS_SetAngle(3, 100, 512) #300
-    up.CDS_SetAngle(4, 930, 512) #730
-    time.sleep(0.5)
-    up.CDS_SetAngle(3, 412, 712) #550  664
-    up.CDS_SetAngle(4, 612, 712) #512  338
     time.sleep(1)
+    up.CDS_SetAngle(3, 541, 512) #300  //减小为向上
+    up.CDS_SetAngle(4, 750, 512) #730  //增大为向上
+    time.sleep(0.8)
+    up.CDS_SetAngle(3, 982, 712) #550  664
+    up.CDS_SetAngle(4, 308, 712) #512  338
+    time.sleep(0.8)
 
 up = uptech.UpTech()
 up.LCD_Open(2)
@@ -28,14 +28,6 @@ up.LCD_SetFont(up.FONT_6X10)
 
 while True:
     down_platform_act()
-    # up.CDS_SetAngle(3, 100, 512) #300
-    # up.CDS_SetAngle(4, 930, 512) #730
-    # time.sleep(0.5)
-    # up.CDS_SetAngle(3, 412, 712) #550  664
-    # up.CDS_SetAngle(4, 612, 712) #512  338
-    # time.sleep(1)
-    # up.CDS_SetSpeed(1, -730)  # 730
-    # up.CDS_SetSpeed(2, -750)  # 750
 
 
 
